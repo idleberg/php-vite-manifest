c4df54fbe128f331350ab2e1465721fed429871e
Jan T. Sott <git@idleberg.com>
2022-02-19 02:36:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Vite Manifest
+
+> A parser for [Vite](https://bandcamp.com/developer) v2 manifest files
+
+[![Packagist](https://flat.badgen.net/packagist/license/idleberg/vite-manifest)](https://www.npmjs.org/package/idleberg/vite-manifest)
+[![Packagist](https://flat.badgen.net/packagist/v/idleberg/vite-manifest)](https://www.npmjs.org/package/idleberg/vite-manifest)
+[![CI](https://img.shields.io/github/workflow/status/idleberg/vite-manifest/CI?style=flat-square)](https://github.com/idleberg/vite-manifest/actions)
+
+## Installation
+
+`composer require idleberg/vite-manifest`
+
+## Usage
+
+Import and initialize the Bandcamp module
+
+```php
+use Idleberg\ViteManifest\ViteManifest;
+
+$vm = new ViteManifest(
+    "patch/to/manifest.json"
+)
+```
+
+### Methods
+
+#### `getManifest()`
+
+Usage: `getManifest()`
+
+Returns the contents of the manifest file as a PHP array
+
+#### `getEntrypoint()`
+
+Usage: `getEntrypoint(fileName)`
+
+Returns the entrypoint from the manifest
+
+<details>
+<summary><strong>Example</strong></summary>
+
+```php
+$entrypoint = $vm->getEntrypoint("index.ts");
+
+echo "<script type=\"module\" src=\"{$entrypoint['url']}\" crossorigin integrity=\"{$entrypoint['hash']}\"></script>";
+```
+
+</details>
+
+#### `getImports()`
+
+Usage: `getImports(fileName)`
+
+Returns imports from the manifest
+
+<details>
+<summary><strong>Example</strong></summary>
+
+```php
+foreach ($vm->getImports("index.ts") as $import) {
+   echo "<link rel=\"modulepreload\" href=\"{$import['url']}\" />";
+}
+```
+
+## License
+
+This work is licensed under [The MIT License](LICENSE)
---
+++ src/ViteManifest.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Copyright 2022 Jan T. Sott
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Idleberg\ViteManifest;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Riimu\Kit\PathJoin\Path;
+
+class ViteManifest
+{
+    private $manifest;
+    protected $basePath;
+
+    public function __construct(string $manifestFile, string $basePath = "")
+    {
+        if (!file_exists($manifestFile)) {
+            throw new \Exception("Manifest file does not exist: {$manifestFile}");
+        }
+
+        try {
+            $this->manifest = json_decode(
+                file_get_contents($manifestFile), true
+            );
+        } catch (\Throwable $th) {
+            throw new \Exception("Failed loading manifest: {$th}");
+        }
+
+        if ($basePath && !file_exists($basePath)) {
+            throw new \Exception("Base path does not exist: {$basePath}");
+        }
+
+        $this->basePath = $basePath;
+    }
+
+    /**
+     * Returns the contents of the manifest file
+     *
+     * @return array
+     */
+    public function getManifest(): array
+    {
+        return $this->manifest;
+    }
+
+    /**
+     * Returns the entrypoint from the manifest
+     *
+     * @param string $entry
+     * @return array
+     */
+    public function getEntrypoint(string $entry): array
+    {
+        return isset($this->manifest[$entry])
+            ? [
+                "hash" => $this->getFileHash($this->manifest[$entry]["file"]),
+                "url"  => $this->getPath($this->manifest[$entry]["file"])
+            ] : "";
+    }
+
+    /**
+     * Returns imports from the manifest
+     *
+     * @param string $entry
+     * @return array
+     */
+    public function getImports(string $entry): array
+    {
+        return array_map(function($imports) {
+            return [
+                "hash" => $this->getFileHash($this->manifest[$imports]["file"]),
+                "url"  => $this->getPath($this->manifest[$imports]["file"])
+            ];
+        }, $this->manifest[$entry]["imports"]);
+    }
+
+    /**
+     * Returns SHA-256 hash of file
+     *
+     * @param string $file
+     * @return string
+     */
+    private function getFileHash(string $file): string
+    {
+        return "sha256-" . base64_encode(
+            openssl_digest(
+                file_get_contents(
+                   $this->getPath($file)
+                ), "sha256", true
+            )
+        );
+    }
+
+    /**
+     * Prepends the base path to the given path.
+     *
+     * @param string $file
+     * @return string
+     */
+    private function getPath(string $file): string {
+        return Path::join($this->basePath, $file);
+    }
+}
