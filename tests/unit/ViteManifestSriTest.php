0503576d67ba38214e0d66af82b2b0bc4383e651
Jan T. Sott <git@idleberg.com>
2024-06-09 08:39:53 +0200

    update tests

--- tests/unit/ViteManifestSriTest.php
+++ tests/unit/ViteManifestSriTest.php
@@ -26,7 +26,7 @@ class ViteManifestSriTest extends \Codeception\Test\Unit
     public function testGetManifest()
     {
         $actual = $this->vm->getManifest();
-        $expected = json_decode('{"demo.ts":{"file":"assets/index.deadbeef.js","src":"demo.ts","isEntry":true,"integrity":"sha256-hK5PvH3PaGbMYq5EuedyA6F5uVkfoEwAznLNThffuZ8="}}', true);
+        $expected = json_decode('{"demo.ts":{"file":"assets/index.deadbeef.js","src":"demo.ts","isEntry":true,"integrity":"sha256-hK5PvH3PaGbMYq5EuedyA6F5uVkfoEwAznLNThffuZ8="},"demo.sha256.ts":{"file":"assets/index.deadbeef.js","src":"demo.sha256.ts","isEntry":true,"integrity":"sha256-hK5PvH3PaGbMYq5EuedyA6F5uVkfoEwAznLNThffuZ8="},"demo.sha384.ts":{"file":"assets/index.deadbeef.js","src":"demo.sha384.ts","isEntry":true,"integrity":"sha384-fWetO954Htoz6cSa6ZLx231UagP8VTXlwaO1g/JisfA9TLZnHPlgPBUwsqrWHjg0"},"demo.sha512.ts":{"file":"assets/index.deadbeef.js","src":"demo.sha512.ts","isEntry":true,"integrity":"sha512-yD2Vb8LCDxC5ingFUTEa50J7EaqoK4xJzwimk2+7PPM9jczPfTDHngkduhYar/pz4dCW7qWIhm0fXFDXm1lL/A=="}}', true);
 
         $this->assertEquals($actual, $expected);
     }
@@ -38,5 +38,32 @@ class ViteManifestSriTest extends \Codeception\Test\Unit
 
         $this->assertFileExists($url);
         $this->assertEquals($hash, "sha256-hK5PvH3PaGbMYq5EuedyA6F5uVkfoEwAznLNThffuZ8=");
+    }
+
+    public function testGetEntryPointSHA256()
+    {
+        $entrypoint = $this->vm->getEntrypoint('demo.sha256.ts');
+        ["url" => $url, "hash" => $hash] = $entrypoint;
+
+        $this->assertFileExists($url);
+        $this->assertEquals($hash, "sha256-hK5PvH3PaGbMYq5EuedyA6F5uVkfoEwAznLNThffuZ8=");
+    }
+
+    public function testGetEntryPointSHA384()
+    {
+        $entrypoint = $this->vm->getEntrypoint('demo.sha384.ts');
+        ["url" => $url, "hash" => $hash] = $entrypoint;
+
+        $this->assertFileExists($url);
+        $this->assertEquals($hash, "sha384-fWetO954Htoz6cSa6ZLx231UagP8VTXlwaO1g/JisfA9TLZnHPlgPBUwsqrWHjg0");
+    }
+
+    public function testGetEntryPointSHA512()
+    {
+        $entrypoint = $this->vm->getEntrypoint('demo.sha512.ts');
+        ["url" => $url, "hash" => $hash] = $entrypoint;
+
+        $this->assertFileExists($url);
+        $this->assertEquals($hash, "sha512-yD2Vb8LCDxC5ingFUTEa50J7EaqoK4xJzwimk2+7PPM9jczPfTDHngkduhYar/pz4dCW7qWIhm0fXFDXm1lL/A==");
     }
 }
